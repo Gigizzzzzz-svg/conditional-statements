0b783f4758a8e66dd5d2af1843642c325fb95b9c
Gigizzzzzz_svg <gigididitonceagain@gmail.com>
2026-04-29 08:06:51 +0300

    My first push

---
+++ javascript/main.js
@@ -0,0 +1,53 @@
+// conditional statements -- control the behaviour in js and determine whether a piece of code can run
+
+//if & else
+// if -- checks for postive condition /true condition
+//else --checks if there is no postive condution
+
+if(condition){
+    body/message
+}
+else{
+    body/message
+}
+
+let age = 18;
+
+if(age > 18) {
+    console.log("You can enter ");
+}
+else {
+    console.log("wewe enda home");
+}
+
+  if(age > 18){
+    console.log("Wacha kijana aingie");
+
+}
+else if (age ==18){
+    console.log("Enda home");
+}
+
+function traffic (color){
+    if(clor == "red"){
+        console.log("STOP");
+    }else if(color == "green"){
+        console.log("GO");
+
+    }else{
+        console.log("Proceed with caution");
+    }
+
+}
+traffic("blue")
+
+// age calculator
+// 1 -18 --allowance
+// 19 -24 -- stipend
+// 25 -65 --salary
+//above65 --pension
+
+
+//promocodes
+// Nairobi -- 25%-- 1000;
+//karibu -- 15% --10000
